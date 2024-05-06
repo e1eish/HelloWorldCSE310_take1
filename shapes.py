c66d3f19b2639713921a2d97e7b78f29de23e85b
Ethan <eleish@byui.edu>
2024-05-06 12:25:09 -0600

    Added calculate_rectangle_area function

--- shapes.py
+++ shapes.py
@@ -2,8 +2,15 @@
 
 # This will calculate the shapes areas.
 
+def calculate_rectangle_area(width, height):
+    area = width * height
+    return area
+
 
 def main():
     print('This is our shapes program')
+
+    rectangle_area = calculate_rectangle_area(10, 20)
+    print(f'The area of the rectangle is: {rectangle_area}')
 
 main()
